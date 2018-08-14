b5fb658a3765a5442af83f2b4138a245370d8df5
Thomas Barregren <thomas@kntnt.se>
2018-08-14 18:28:44 +0200

    Working plugin.

---
+++ README.md
@@ -0,0 +1,41 @@
+# Kntnt's Exclude Duplicated Posts for Beaver Builder
+
+Makes it possible to avoid duplicated posts when using Beaver Builder custom query.
+
+## Description
+
+The Beaver Builder plugin provides several post modules that uses custom query as source. If you use any of these modules on posts, you can exclude the current post from the query. This plugin extends that option to exclude all posts returned by previous instances of any of these modules on the post.
+
+Thus, you only need this plugin if you intend to put more than one post module on a page or layout and want tp avoid duplicated posts.
+
+## Installation
+
+Install the plugin [the usually way](https://codex.wordpress.org/Managing_Plugins#Installing_Plugins).
+
+## Frequently Asked Questions
+
+### Where is the setting page?
+
+There is no setting page.
+
+### How can I get help?
+
+If you have a questions about the plugin, and cannot find an answer here, start by looking at [issues](https://github.com/Kntnt/kntnt-bb-loop-excluder/issues) and [pull requests](https://github.com/Kntnt/kntnt-bb-loop-excluder/pulls). If you still cannot find the answer, feel free to ask in the the plugin's [issue tracker](https://github.com/Kntnt/kntnt-bb-loop-excluder/issues) at Github.
+
+### How can I report a bug?
+
+If you have found a potential bug, please report it on the plugin's [issue tracker](https://github.com/Kntnt/kntnt-bb-loop-excluder/issues) at Github.
+
+### How can I contribute?
+
+Contributions to the code or documentation are much appreciated.
+
+If you are unfamiliar with Git, please date it as a new issue on the plugin's [issue tracker](https://github.com/Kntnt/kntnt-bb-loop-excluder/issues) at Github.
+
+If you are familiar with Git, please do a pull request.
+
+## Changelog
+
+### 1.0.0
+
+Initial release. Fully functional plugin.
---
+++ kntnt-bb-loop-excluder.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Kntnt's Exclude Duplicated Posts for Beaver Builder
+ * Plugin URI:        https://github.com/Kntnt/kntnt-bb-loop-excluder
+ * Description:       Makes it possible to avoid duplicated posts when using Beaver Builder custom query.
+ * Version:           1.0.0
+ * Author:            Thomas Barregren
+ * Author URI:        https://www.kntnt.com/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       kntnt-bb-loop-excluder
+ * Domain Path:       /languages
+ */
+
+namespace Kntnt\BB_Loop_Excluder;
+
+defined( 'WPINC' ) && new Plugin();
+
+final class Plugin {
+
+  	private $pids = [];
+
+    public function __construct() {
+      add_action( 'plugins_loaded', [ $this, 'run' ] );
+    }
+
+    public function run() {
+  		add_filter( 'fl_builder_render_settings_field', [ $this, 'builder_render_settings_field' ], 10, 3 );
+  		add_filter( 'fl_builder_loop_query_args', [ $this, 'builder_loop_query_args' ] );
+  		add_filter( 'fl_builder_loop_query', [ $this, 'builder_loop_query' ], 10, 2 );
+  	}
+
+  	public function builder_render_settings_field( $field, $name, $settings ) {
+
+  		if ( 'exclude_self' == $name ) {
+  			$field['label'] = __( 'Exclude duplicates', 'kntnt-bb-loop-excluder' );
+  			$field['help']  = __( 'Exclude all previous returned posts from this query.', 'kntnt-bb-loop-excluder' );
+  		}
+
+  		return $field;
+
+  	}
+
+  	public function builder_loop_query_args( $args ) {
+
+  		if ( $this->pids && isset( $args['settings']->exclude_self ) && 'yes' == $args['settings']->exclude_self ) {
+  			$args['post__not_in'] = isset( $args['post__not_in'] ) ? $args['post__not_in'] + $this->pids : $this->pids;
+  		}
+
+  		return $args;
+
+  	}
+
+  	public function builder_loop_query( $query, $settings ) {
+
+  		foreach ( $query->posts as $post ) {
+  			$this->pids[] = $post->ID;
+  		}
+
+  		return $query;
+
+  	}
+
+}
