a2cdfc36002e56f5db4f80521e352b77773e14ab
Thomas Barregren <thomas@kntnt.se>
2018-08-20 11:50:21 +0200

    Coreccted two mistakes.

--- kntnt-bb-loop-excluder.php
+++ kntnt-bb-loop-excluder.php
@@ -20,47 +20,49 @@ defined( 'WPINC' ) && new Plugin();
 
 final class Plugin {
 
-  	private $pids = [];
+	private $pids = [];
 
-    public function __construct() {
-      add_action( 'plugins_loaded', [ $this, 'run' ] );
-    }
+	public function __construct() {
+		add_action( 'plugins_loaded', [ $this, 'run' ] );
+	}
 
-    public function run() {
-  		add_filter( 'fl_builder_render_settings_field', [ $this, 'builder_render_settings_field' ], 10, 3 );
-  		add_filter( 'fl_builder_loop_query_args', [ $this, 'builder_loop_query_args' ] );
-  		add_filter( 'fl_builder_loop_query', [ $this, 'builder_loop_query' ], 10, 2 );
-  	}
+	public function run() {
+		load_plugin_textdomain( 'kntnt-bb-loop-excluder', false, basename( dirname( __FILE__ ) ) . '/languages' );
+		add_filter( 'fl_builder_render_settings_field', [ $this, 'builder_render_settings_field' ], 10, 3 );
+		add_filter( 'fl_builder_loop_query_args', [ $this, 'builder_loop_query_args' ] );
+		add_filter( 'fl_builder_loop_query', [ $this, 'builder_loop_query' ], 10, 2 );
+	}
 
-  	public function builder_render_settings_field( $field, $name, $settings ) {
+	public function builder_render_settings_field( $field, $name, $settings ) {
 
-  		if ( 'exclude_self' == $name ) {
-  			$field['label'] = __( 'Exclude duplicates', 'kntnt-bb-loop-excluder' );
-  			$field['help']  = __( 'Exclude all previous returned posts from this query.', 'kntnt-bb-loop-excluder' );
-  		}
+		if ( 'exclude_self' == $name ) {
+			$field['label'] = __( 'Exclude duplicates', 'kntnt-bb-loop-excluder' );
+			$field['help'] = __( 'Exclude all previous returned posts from this query.', 'kntnt-bb-loop-excluder' );
+		}
 
-  		return $field;
+		return $field;
 
-  	}
+	}
 
-  	public function builder_loop_query_args( $args ) {
+	public function builder_loop_query_args( $args ) {
 
-  		if ( $this->pids && isset( $args['settings']->exclude_self ) && 'yes' == $args['settings']->exclude_self ) {
-  			$args['post__not_in'] = isset( $args['post__not_in'] ) ? $args['post__not_in'] + $this->pids : $this->pids;
-  		}
+		if ( $this->pids && isset( $args['settings']->exclude_self ) && 'yes' == $args['settings']->exclude_self ) {
+			// Current post id is added to $args['post__not_in'] before this filter is called.
+			$args['post__not_in'] = array_merge( $this->pids, $args['post__not_in'] );
+		}
 
-  		return $args;
+		return $args;
 
-  	}
+	}
 
-  	public function builder_loop_query( $query, $settings ) {
+	public function builder_loop_query( $query, $settings ) {
 
-  		foreach ( $query->posts as $post ) {
-  			$this->pids[] = $post->ID;
-  		}
+		foreach ( $query->posts as $post ) {
+			$this->pids[] = $post->ID;
+		}
 
-  		return $query;
+		return $query;
 
-  	}
+	}
 
 }
